e9091768258d8ede5778b3e0443d2b6499b5e702
David Strauß <mail@davidstrauss.at>
2021-11-24 15:24:38 +0100

    add action which maps workflow inputs to an array usable in matrix config

---
+++ .github/actions/map-inputs/index.js
@@ -0,0 +1,51 @@
+//@ts-check
+const core = require('@actions/core');
+
+try {
+  const buildAndroid = core.getInput('android');
+  const buildIos = core.getInput('ios');
+  const distributeTest = core.getInput('test');
+  const publishRelease = core.getInput('release');
+
+  console.log({ buildAndroid, buildIos, distributeTest, publishRelease });
+
+  if (!buildAndroid && !buildIos) {
+    throw new Error('No platform selected, exiting');
+  }
+
+  if (!distributeTest && !publishRelease) {
+    throw new Error('No release type selected, exiting');
+  }
+
+  const platforms = [];
+  const types = [];
+
+  if (buildAndroid) {
+    platforms.push('android');
+  }
+
+  if (buildIos) {
+    platforms.push('ios');
+  }
+
+  if (distributeTest) {
+    types.push({
+      type: 'test',
+      lane: 'app_center_release_all'
+    });
+  }
+
+  if (publishRelease) {
+    types.push({
+      type: 'release',
+      lane: 'store_release'
+    });
+  }
+
+  console.log({ platforms, types });
+
+  core.setOutput('platforms', platforms);
+  core.setOutput('types', types);
+} catch (error) {
+  core.setFailed(error.message);
+}
